412c989e8c878d91d2fd89615174909a2c6388a7
ebnanzhran <bnanzhran515@gmail.com>
2022-06-29 10:25:04 +0300

    message done

--- src/pilot/pilot.js
+++ src/pilot/pilot.js
@@ -9,19 +9,26 @@ let ID = faker.datatype.uuid();
 const events_airline = io.connect(`${host}/airline`);
 const events = io.connect(host);
 
+
+events.emit('get-all');
+
 events_airline.on('new-flight', () => {
   setTimeout(() => {
     let tookOff = `Pilot: flight with ID ${ID} took-off`;
     console.log(tookOff);
-    events_airline.emit('took-off', tookOff);
+    events_airline.emit('took-off');
   }, 4000);
 });
 events.on('new-flight', () => {
   setTimeout(() => {
     let arrived = `Pilot: flight with ID ${ID} has arrived`;
     console.log(arrived);
-    events.emit('Arrived', arrived);
+    events.emit('Arrived');
   }, 7000);
 });
-
-
+events.on('flight', (payload) => {
+  console.log(payload);
+  Object.keys(payload).forEach((key) => {
+    console.log(`Pilot:Sorry i didn't catch this flight ID ${key}.`);
+  });
+})
